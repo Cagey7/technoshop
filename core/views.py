e8bcee464920e37607543eca015aa63e1ec7d002
Cagey7 <dagodecagey@gmail.com>
2023-12-18 16:33:01 +0600

    Added basic navbar

--- core/views.py
+++ core/views.py
@@ -2,8 +2,23 @@ from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
 
 
+navbar_auth = [
+    {"title":"Главная страница", "url_name": "index", "selected": 0},
+    {"title":"Логин", "url_name": "login", "selected": 0},
+    {"title":"Регистрация", "url_name": "register", "selected": 0},
+]
+navbar_not_auth = [
+    {"title":"Главная страница", "url_name": "index", "selected": 0},
+    {"title":"Выйти", "url_name": "logout", "selected": 0}
+]
+
+
 def index(request):
-    return render(request, "core/index.html")
+    context = {
+        "navbar_auth": navbar_auth,
+        "navbar_not_auth": navbar_not_auth
+    }
+    return render(request, "core/index.html", context=context)
 
 
 def page_not_found(request, exception):
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
+from core.views import navbar_auth, navbar_not_auth
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView
 from .forms import *
@@ -8,9 +8,17 @@ from .forms import *
 class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name="users/login.html"
-
+    redirect_authenticated_user = True
+    extra_context = {
+        "navbar_auth": navbar_auth,
+        "navbar_not_auth": navbar_not_auth
+    }
 
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = "users/register.html"
     success_url = reverse_lazy("login")
+    extra_context = {
+        "navbar_auth": navbar_auth,
+        "navbar_not_auth": navbar_not_auth
+    }
