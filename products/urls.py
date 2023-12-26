7ee99a385ab677cf991716849b312d8d82ee1ce4
Cagey7 <dagodecagey@gmail.com>
2023-12-26 14:17:49 +0600

    Added item page

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.ProductsHome.as_view(), name="index"),
     path("category/<slug:category_slug>", views.ItemCategory.as_view(), name="category"),
+    path("item/<slug:item_slug>", views.ShowItem.as_view(), name="item"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.db.models.base import Model as Model
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import *
 from core.views import navbar_auth, navbar_not_auth
 
@@ -21,6 +23,7 @@ class ProductsHome(ListView):
 class ItemCategory(ListView):
     template_name = "products/index.html"
     context_object_name = "items"
+    allow_empty = False
     extra_context = {
         "navbar_auth": navbar_auth,
         "navbar_not_auth": navbar_not_auth,
@@ -36,3 +39,22 @@ class ItemCategory(ListView):
         context["title"] = "Категория - " + category.name
         context["selected_category"] = category.pk
         return context
+
+
+class ShowItem(DetailView):
+    model = Item
+    template_name = "products/item.html"
+    slug_url_kwarg = "item_slug"
+    context_object_name = "item"
+    extra_context = {
+        "navbar_auth": navbar_auth,
+        "navbar_not_auth": navbar_not_auth
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = context["item"].name
+        return context
+    
+    def get_object(self, queryset=None):
+        return get_object_or_404(Item.published, slug=self.kwargs[self.slug_url_kwarg])
